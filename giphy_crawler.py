a3c4f2cf940fbd3b83e9fc0e14786ff3549a6526
cspidermx <cspidermx@gmail.com>
2018-04-11 21:58:05 -0500

    crawl giphy

--- giphy_crawler.py
+++ giphy_crawler.py
@@ -1,28 +1,10 @@
 import csv
 import bs4 as bs
 import urllib.request
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-import os
-
-
-def getdynamic(url):
-    # instantiate a chrome options object so you can set the size and headless preference
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--window-size=1920x1080")
-    # download the chrome driver from https://sites.google.com/a/chromium.org/chromedriver/downloads and put it in the
-    # current directory
-    chrome_driver = os.getcwd() + "\\chromedriver.exe"
-
-    driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
-    try:
-        driver.get(url)
-        data = driver.page_source
-        driver.close()
-        return bs.BeautifulSoup(data, "lxml")
-    except:
-        return None
+import json
+from dateutil import parser
+from dateutil.tz import gettz
+from datetime import datetime
 
 
 def __apnd (txt, frst, scnd, epty, start):
@@ -42,7 +24,7 @@ def __apnd (txt, frst, scnd, epty, start):
 
 with open('hyperrpg20180411.csv') as csvfile:
     reader = csv.DictReader(csvfile)
-    fieldnames = ['GIF URL', 'VIEWS', 'TAGS']
+    fieldnames = ['GIF URL', 'VIEWS', 'RATING', 'ULD', 'ULM', 'ULY', 'ULT', 'TRD', 'TRM', 'TRY', 'TRT', 'TAGS']
     csvfile2 = open('hyperrpgTAGS.csv', 'w', newline='')
     writer = csv.DictWriter(csvfile2, fieldnames=fieldnames)
     writer.writeheader()
@@ -51,22 +33,36 @@ with open('hyperrpg20180411.csv') as csvfile:
     row_count = len(data)
     csvfile.seek(0)
     reader = csv.DictReader(csvfile)
-    i=0
+    i = 0
+    count = 0
+    delgifs = 0
+    tzinfos = {"CST": gettz("America/Mexico_City")}
     for row in reader:
         i += 1
         print('(', i, '/', row_count, ')')
         idGIF = __apnd(row['Gif URL'], 'https://media.giphy.com/media/', '/giphy.gif', '', 0)
-        # gifurl = row['Gif URL']
         gifurl = 'https://giphy.com/gifs/' + idGIF[0] + '/html5'
         apiurl = 'https://api.giphy.com/v1/gifs/' + idGIF[0] + '?api_key=8I9MzBtUTMD4ObOS7BqF9DqnYS7MGZ9k'
         if row['Is Public'] == 'true':
             try:
                 sauce = urllib.request.urlopen(apiurl).read()
+                metadata = json.loads(sauce)
+                rating = metadata['data']['rating']
+                upload = metadata['data']['import_datetime']
+                upload = parser.parse(upload, tzinfos=tzinfos).strftime('%d/%m/%Y %H:%M:%S')
+                upldate = datetime.strptime(upload, '%d/%m/%Y %H:%M:%S')
+                trend = metadata['data']['trending_datetime']
+                try:
+                    trend = parser.parse(trend, tzinfos=tzinfos).strftime('%d/%m/%Y %H:%M:%S')
+                    tredate = datetime.strptime(trend, '%d/%m/%Y %H:%M:%S')
+                except:
+                    tredate = datetime(2000, 1, 1)
             except urllib.error.URLError:
                 sauce = None
+                count += int(row['View Count'])
+                delgifs += 1
             if sauce is not None:
                 try:
-                    # soup = getdynamic(gifurl)
                     sauce = urllib.request.urlopen(gifurl).read()
                     soup = bs.BeautifulSoup(sauce, "lxml")
                 except urllib.error.URLError:
@@ -75,15 +71,44 @@ with open('hyperrpg20180411.csv') as csvfile:
                     exists = "TRUE"
                 else:
                     exist = "FALSE"
-                # print(idGIF[0], row['View Count'], gifurl, exists)  # Gif URL	Is Public	View Count
+
                 if soup is not None:
                     for mt in soup.find_all('meta'):
                         if 'name' in mt.attrs:
                             if mt['name'] == 'keywords':
-                                # ['GIF URL', 'VIEWS', 'TAGS']
-                                print({'GIF URL': row['Gif URL'], 'VIEWS': row['View Count'], 'TAGS': mt['content'].replace(', ', '|')})
-                                writer.writerow({'GIF URL': row['Gif URL'], 'VIEWS': row['View Count'], 'TAGS': mt['content'].replace(', ', '|')})
-                                # print(mt['content'].replace(', ', '|'))
+                                # ['GIF URL', 'VIEWS', 'RATING',
+                                # 'ULD', 'ULM', 'ULY', 'ULT',
+                                # 'TRD', 'TRM', 'TRY', 'TRT',
+                                # 'TAGS']
+                                print({'GIF URL': row['Gif URL'],
+                                       'VIEWS': row['View Count'],
+                                       'RATING': rating,
+                                       'ULD': upldate.day,
+                                       'ULM': upldate.month,
+                                       'ULY': upldate.year,
+                                       'ULT': str(upldate.time()),
+                                       'TRD': tredate.day,
+                                       'TRM': tredate.month,
+                                       'TRY': tredate.year,
+                                       'TRT': str(tredate.time()),
+                                       'TAGS': mt['content'].replace(', ', '|')})
+                                writer.writerow({'GIF URL': row['Gif URL'],
+                                                 'VIEWS': row['View Count'],
+                                                 'RATING': rating,
+                                                 'ULD': upldate.day,
+                                                 'ULM': upldate.month,
+                                                 'ULY': upldate.year,
+                                                 'ULT': str(upldate.time()),
+                                                 'TRD': tredate.day,
+                                                 'TRM': tredate.month,
+                                                 'TRY': tredate.year,
+                                                 'TRT': str(tredate.time()),
+                                                 'TAGS': mt['content'].replace(', ', '|')})
+        else:
+            count += int(row['View Count'])
+            delgifs += 1
+    print({'GIF URL': 'ERASED GIFS', 'VIEWS': count, 'RATING': delgifs})
+    writer.writerow({'GIF URL': 'ERASED GIFS', 'VIEWS': count, 'RATING': delgifs})
     csvfile2.close()
 
 
