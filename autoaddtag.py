41e3136b5022eea17a8e2b278487529e5048f3e2
cspidermx <cspidermx@gmail.com>
2018-11-22 22:25:03 -0600

    data

--- autoaddtag.py
+++ autoaddtag.py
@@ -30,29 +30,39 @@ btnlgn.click()
 wait = WebDriverWait(driver, 20)
 wait.until(EC.visibility_of_element_located((By.ID, "upload-avatar")))
 
+i = 0
+with open('list.txt') as f:
+    for i, l in enumerate(f):
+        pass
+total = i + 1
+
+i = 1
 with open('list.txt', 'r') as file:
     for line in file.readlines():
         url = line.replace('\n', '')
         driver.get(url)
-        wait = WebDriverWait(driver, 10)
+        wait = WebDriverWait(driver, 15)
         wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "_14glt1AeSjnOdEg6jPXf4y")))
         gif = driver.find_element_by_class_name("KRS9L9BsuEdhF-ACKiX8x")
         hover = ActionChains(driver).move_to_element(gif)
         hover.perform()
-        wait = WebDriverWait(driver, 10)
+        wait = WebDriverWait(driver, 15)
         wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "_3VnuN1NZurY9lA0b_kRnUu")))
         btnedit = driver.find_element_by_class_name("_3VnuN1NZurY9lA0b_kRnUu")
         btnedit.click()
-        wait = WebDriverWait(driver, 10)
+        wait = WebDriverWait(driver, 15)
         wait.until(EC.visibility_of_element_located((By.CLASS_NAME, "Fx76GcH4nzz12sVCkdN1_")))
-        addtag = driver.find_element_by_xpath("(//input[@class='Fx76GcH4nzz12sVCkdN1_ _2-GxqWYNxZ-IXQRDqN-Soq _1bc7yOoPoF9ZAns5laPqC0'])[2]")
+        addtag = driver.find_element_by_xpath("(//input[@class='Fx76GcH4nzz12sVCkdN1_ _2-GxqWYNxZ-IXQRDqN-Soq"
+                                              " _1bc7yOoPoF9ZAns5laPqC0'])[2]")
         addtag.click()
-        addtag.send_keys('zaceubank')
+        addtag.send_keys('themattacevedo')
         addtag.send_keys(' ')
         addtag.send_keys(Keys.ENTER)
         addtag.send_keys(Keys.DELETE)
         time.sleep(5)
         btnclose = driver.find_element_by_xpath("//div[@class='_2yA57V8LtMT5O9b9rPuLM1 ss-delete']")
         btnclose.click()
+        print("({}/{})".format(i, total), url)
+        i += 1
 
 driver.close()
